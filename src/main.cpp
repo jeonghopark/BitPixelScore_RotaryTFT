fc3fab171cec6f84704e0adaeffe54b3608cde68
jeonghopark <jeongho.park@me.com>
2016-07-29 13:11:24 +0200

    postion windows

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,8 @@ int main( ){
     
     ofGLFWWindowSettings settings;
     settings.windowMode = OF_WINDOW;
-    ofVec2f _v = ofVec2f(1280, 0);
-    settings.setPosition(_v);
+    //ofVec2f _v = ofVec2f(1280, 0);
+    //settings.setPosition(_v);
     settings.width = 1024;
     settings.height = 768;
     
